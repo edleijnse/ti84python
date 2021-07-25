122dc820ba29a300027c81dd095470afdac1b0a2
Ed Leijnse <ed@leijnse.info>
2021-07-25 15:30:47 +0200

    Mastermind option tr replay game added

--- tests/MASTR002.py
+++ tests/MASTR002.py
@@ -44,159 +44,171 @@ def print_mastermind_board(passcode, guess_codes, guess_flags):
     print(dottedline)
 
 
+# Function to set the guess_flags
 def setGuessflags(guess_flags, turn, code, passcode):
     ii = 0
-    passcodeItemChecked=[]
+    passcodeItemChecked = []
     # check code and position
     for x in code:
-        if (ii==turn):
-           cc=0
-           for codeitem in x:
-               pp=0
-               # check code and position
-               for passcodeitem in passcode:
-                   # check if passworditem is already processed
-                   toprocess = True
-                   for pitem in passcodeItemChecked:
-                       if pitem == pp:
-                           toprocess = False
-                   if toprocess == True:
-                      if (str(codeitem)==str(passcodeitem)):
-                         if cc==pp:
-                            guess_flags[turn][cc] = 'R'
-                            passcodeItemChecked.append(pp)
-                         break
-                   pp +=1
-               cc +=1
+        if (ii == turn):
+            cc = 0
+            for codeitem in x:
+                pp = 0
+                # check code and position
+                for passcodeitem in passcode:
+                    # check if passworditem is already processed
+                    toprocess = True
+                    for pitem in passcodeItemChecked:
+                        if pitem == pp:
+                            toprocess = False
+                    if toprocess == True:
+                        if (str(codeitem) == str(passcodeitem)):
+                            if cc == pp:
+                                guess_flags[turn][cc] = 'R'
+                                passcodeItemChecked.append(pp)
+                            break
+                    pp += 1
+                cc += 1
         ii += 1
     ii = 0
     # check code independent of position
     for x in code:
         if (ii == turn):
-           cc = 0
-           for codeitem in x:
-               pp = 0
-               # check code
-               for passcodeitem in passcode:
-                   # check if passworditem is already processed
-                   toprocess = True
-                   for pitem in passcodeItemChecked:
-                       if pitem == pp:
-                           toprocess = False
-                   if toprocess == True:
-                       if (str(codeitem) == str(passcodeitem)):
-                           if cc == pp:
-                               guess_flags[turn][cc] = 'R'
-                           else:
-                               guess_flags[turn][cc] = 'W'
-                           passcodeItemChecked.append(pp)
-                           break
-                   pp += 1
-               cc +=1
-        ii +=1
+            cc = 0
+            for codeitem in x:
+                pp = 0
+                # check code
+                for passcodeitem in passcode:
+                    # check if passworditem is already processed
+                    toprocess = True
+                    for pitem in passcodeItemChecked:
+                        if pitem == pp:
+                            toprocess = False
+                    if toprocess == True:
+                        if (str(codeitem) == str(passcodeitem)):
+                            if cc == pp:
+                                guess_flags[turn][cc] = 'R'
+                            else:
+                                guess_flags[turn][cc] = 'W'
+                            passcodeItemChecked.append(pp)
+                            break
+                    pp += 1
+                cc += 1
+        ii += 1
     return guess_flags
 
 
 # The Main function
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-   # List of colors
-   colors = ["1RED", "2GREEN", "3YELLOW", "4BLUE", "5BLACK", "6ORANGE"]
+def playthegame():
+    # List of colors
+    colors = ["1RED", "2GREEN", "3YELLOW", "4BLUE", "5BLACK", "6ORANGE"]
 
-   # Mapping of colors to numbers
-   colors_map = {1: "1RED", 2: "2GREEN", 3: "3YELLOW", 4: "4BLUE", 5: "5BLACK", 6: "6ORANGE"}
+    # Mapping of colors to numbers
+    colors_map = {1: "1RED", 2: "2GREEN", 3: "3YELLOW", 4: "4BLUE", 5: "5BLACK", 6: "6ORANGE"}
 
-   # Randomly selecting a passcode
-   # to be rewritten for TI-84
-   # https://www.w3schools.com/python/ref_random_randrange.asp
-   # random.shuffle(colors)
-   col1 = random.randrange(1, 6)
-   col2 = random.randrange(1, 6)
-   col3 = random.randrange(1, 6)
-   col4 = random.randrange(1, 6)
-   # passcode = colors[:4]
-   passcode = []
-   passcode.clear()
-   passcode.append(colors[col1])
-   passcode.append(colors[col2])
-   passcode.append(colors[col3])
-   passcode.append(colors[col4])
+    # Randomly selecting a passcode
+    # to be rewritten for TI-84
+    # https://www.w3schools.com/python/ref_random_randrange.asp
+    # random.shuffle(colors)
+    col1 = random.randrange(1, 6)
+    col2 = random.randrange(1, 6)
+    col3 = random.randrange(1, 6)
+    col4 = random.randrange(1, 6)
+    # passcode = colors[:4]
+    passcode = []
+    passcode.clear()
+    passcode.append(colors[col1])
+    passcode.append(colors[col2])
+    passcode.append(colors[col3])
+    passcode.append(colors[col4])
 
-   # Number of chances for the player
-   chances = 8
+    # Number of chances for the player
+    chances = 8
 
-   # The passcode to be shown to the user
-   show_passcode = ['UNK', 'UNK', 'UNK', 'UNK']
+    # The passcode to be shown to the user
+    show_passcode = ['UNK', 'UNK', 'UNK', 'UNK']
 
-   # The codes guessed by the player each turn
-   guess_codes = [['-', '-', '-', '-'] for x in range(chances)]
+    # The codes guessed by the player each turn
+    guess_codes = [['-', '-', '-', '-'] for x in range(chances)]
 
-   # The clues provided to the player each turn
-   guess_flags = [['-', '-', '-', '-'] for x in range(chances)]
+    # The clues provided to the player each turn
+    guess_flags = [['-', '-', '-', '-'] for x in range(chances)]
 
-   clear()
+    clear()
 
-   # The current turn
-   turn = 0
+    # The current turn
+    turn = 0
 
-   # The GAME LOOP
-   while turn < chances:
+    # The GAME LOOP
+    while turn < chances:
 
-       print(dottedline)
-       print("\t\tMenu")
-       print(dottedline)
-       print("Enter code using numbers.")
-       print("1 - RED, 2 - GREEN, 3 - YELLOW, 4 - BLUE, 5 - BLACK, 6 - ORANGE")
-       print("Example: RED YELLOW ORANGE BLACK ---> 1 3 6 5")
-       print(dottedline)
-       print_mastermind_board(passcode, guess_codes, guess_flags)
+        print(dottedline)
+        print("\t\tMenu")
+        print(dottedline)
+        print("Enter code using numbers.")
+        print("1 - RED, 2 - GREEN, 3 - YELLOW, 4 - BLUE, 5 - BLACK, 6 - ORANGE")
+        print("Example: RED YELLOW ORANGE BLACK ---> 1 3 6 5")
+        print(dottedline)
+        print_mastermind_board(passcode, guess_codes, guess_flags)
 
-       # Accepting the player input
-       try:
-           code = list(map(int, input("Enter choice (.) = ").split(".")))
-       except ValueError:
-           clear()
-           print("\tWrong choice!! Try again!!")
-           continue
+        # Accepting the player input
+        try:
+            code = list(map(int, input("Enter choice (.) = ").split(".")))
+        except ValueError:
+            clear()
+            print("\tWrong choice!! Try again!!")
+            continue
 
-           # Check if the number of colors nunbers are 4
-       if len(code) != 4:
-           clear()
-           print("\tWrong choice!! Try again!!")
-           continue
+            # Check if the number of colors nunbers are 4
+        if len(code) != 4:
+            clear()
+            print("\tWrong choice!! Try again!!")
+            continue
 
-       # Check if each number entered corresponds to a number
-       flag = 0
-       for x in code:
-           if x > 6 or x < 1:
-               flag = 1
+        # Check if each number entered corresponds to a number
+        flag = 0
+        for x in code:
+            if x > 6 or x < 1:
+                flag = 1
 
-       if flag == 1:
-           clear()
-           print("\tWrong choice!! Try again!!")
-           continue
+        if flag == 1:
+            clear()
+            print("\tWrong choice!! Try again!!")
+            continue
 
-           # Storing the player input
-       for i in range(4):
-           guess_codes[turn][i] = colors_map[code[i]]
+            # Storing the player input
+        for i in range(4):
+            guess_codes[turn][i] = colors_map[code[i]]
 
-           # Process to apply clues according to the player input
+            # Process to apply clues according to the player input
 
-       guess_flags = setGuessflags(guess_flags, turn, code,  passcode)
+        guess_flags = setGuessflags(guess_flags, turn, guess_codes, passcode)
 
-       # Check for win condition
-       if guess_codes[turn] == passcode:
-           clear()
-           print_mastermind_board(passcode, guess_codes, guess_flags)
-           print("YOU WIN!!!!")
-           break
+        # Check for win condition
+        if guess_codes[turn] == passcode:
+            clear()
+            print_mastermind_board(passcode, guess_codes, guess_flags)
+            print("YOU WIN!!!!")
+            break
+        # Update turn
+        turn += 1
+        clear()
 
-       # Update turn
-       turn += 1
-       clear()
+    # Check for loss condiiton
+    if turn == chances:
+        clear()
+        print_mastermind_board(passcode, guess_codes, guess_flags)
+        print("YOU LOSE!!!")
 
-   # Check for loss condiiton
-   if turn == chances:
-       clear()
-       print_mastermind_board(passcode, guess_codes, guess_flags)
-       print("YOU LOSE!!!")
+
+goon = True
+while goon == True:
+    playthegame()
+    askgoon = str(input("next game enter 1: "))
+    if (askgoon=="1"):
+        goon=True
+    else:
+        goon=False
+print ("bye for now!")
